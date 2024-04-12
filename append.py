898985679db3c3f4d0e49b758f99dd7811a6e782
Sundaresh-Sankaran_sasinst <sundaresh.sankaran@sas.com>
2024-04-12 16:38:47 -0400

    test mod append.py

--- append.py
+++ append.py
@@ -2,6 +2,9 @@ import os
 code_dir = os.path.join(os.getcwd(),"files")
 code_list = [f for f in os.listdir(code_dir) if os.path.isfile(os.path.join(code_dir, f)) and f.endswith("file")]
 
+print(os.getcwd())
+print(list(os.listdir(code_dir)))
+
 with open(os.path.join(os.getcwd(),"These_Are_All.files"), 'w') as outfile:
     for fname in code_list:
         with open(os.path.join(code_dir,fname)) as infile:
